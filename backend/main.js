e20dac95b7e45fcaf3e2bc32f2c8e41d17838afd
Tubokeyi Micheal <mtubokeyi@gmail.com>
2025-04-14 14:15:07 +0100

    my frontend commit

--- backend/main.js
+++ backend/main.js
@@ -22,7 +22,7 @@ app.use(cors({
 
 app.use(express.json());
 app.use(cookieParser(process.env.COOKIE_SECRET));
-
+ 
 // Logging (disable in production for performance)
 if (process.env.NODE_ENV !== 'production') {
   app.use(morgan('dev'));
